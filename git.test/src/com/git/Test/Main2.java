5e90d507eb96b1ef2fb9789d30646a7faf4aeae4
user1 <user1@user1-PC>
2018-12-19 14:55:10 +0900

    Main2 hobby추가!

--- git.test/src/com/git/Test/Main2.java
+++ git.test/src/com/git/Test/Main2.java
@@ -3,5 +3,17 @@ package com.git.Test;
 public class Main2 {
 	
 	private int age;
+	private String hobby;
+	
+	
+	public String getHobby()
+	{
+		return hobby;
+	}
+	
+	public int getAge()
+	{
+		return age;
+	}
 
 }
